17b7f59c5dc85a324a65d9195e35e9d9f69e40ef
Laniakea <ELT_Core@163.com>
2025-11-12 22:28:50 +0800

    重新调整资产

---
+++ Source/UFlappyBird/UFlappyBird.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UFlappyBird.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UFlappyBird, "UFlappyBird" );
